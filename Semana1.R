5a731568be66f182bc9c3c0f71f204c88f54bc88
tcastrop <85913700+tcastrop@users.noreply.github.com>
2021-07-12 06:55:19 -0600

    Add files via upload

---
+++ Semana1.R
@@ -0,0 +1,115 @@
+#Semana 1 
+#Tipo dato 
+
+
+#Variable (Nombre = / <- valor) 
+#espacio en memoria donde se almacena el dato
+
+#integer, double, String, logical(True - False)
+
+#integer
+VariableInteger = 2L
+typeof(VariableInteger)
+
+#Double 
+variableDouble =2.4
+typeof(variableDouble)
+
+#String
+VariableString ="Brayner"
+
+#logical 
+variebleLogical = TRUE
+
+#typeof / class
+
+#Operadores 
+#Aritmeticos 
+operador=  14
+operador
+
+#Relacionales
+5<3
+5<=3
+5>3
+5==7
+5!=4
+
+#Asignacion 
+
+x =3
+y<-4
+
+#Practica 
+
+Mango = 10
+Manzana = 5
+Aguacate  = 2*15
+
+(Aguacate)+(4*Manzana)+(Mango+Mango/2)
+
+
+ #Calculo de hipotenusa 
+  
+  a=3
+  b=4
+  
+  c= sqrt(a^2+b^2)
+  
+  paste("Valor de c es:",c)
+  
+  
+#Estructura de control 
+#iF
+  
+  edad = 100
+  
+   if (edad >= 18) {
+  
+     print("Mayor de edad")
+  
+   }else{
+  
+     print("Menor de edad")
+  
+   }
+
+#For 
+  final=10
+  
+  for (i in 1:final) {
+  
+     if (i>7) {
+       print("Soy mayor 7")
+     }
+  
+  }
+  
+  #Vector 
+  
+  V = c(1,2,3,4,5,6,7,6.5)
+  V[7]
+  
+  c(9,3,4,5,6,7,8) -> v3
+  
+  PrimerVector = c(1, 2, 3, 4, 5, 6, 7, 8)
+  
+  assign("v4", c(1,2,3,4,5,6))
+  
+ V/PrimerVector
+  
+  
+  
+ sucesiones =  1:100   
+ 
+ seq(1,100) #mismo efecto que 3:10
+ 
+ seq(1,100, by=30) #saltando de 3 en 3
+ 
+ rep(1:4,5) #repetimos 1:4 dos veces
+  
+V
+
+length(V)
+
+ 
