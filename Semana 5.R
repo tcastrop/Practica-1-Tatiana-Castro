cd75f46875b56e95d661fde108e0faf97c8c733f
tcastrop <85913700+tcastrop@users.noreply.github.com>
2021-07-12 06:53:49 -0600

    Add files via upload

---
+++ Semana 5.R
@@ -0,0 +1,35 @@
+library("DBI")
+library("odbc")
+library("dbplyr")
+
+DB_BikeStores <- DBI::dbConnect(odbc::odbc(),
+                                Driver = "SQL Server",
+                                Server = "dbtedamssql.uh.ac.cr",
+                                Database = "BikeStores",
+                                UID = rstudioapi::askForPassword("Database user"),
+                                PWD = rstudioapi::askForPassword("Database password"),
+                                Port = 1433)
+
+Stores<-dbGetQuery(DB_BikeStores,"select * from [sales].[order_items]")
+View(Stores)
+
+#PRACTICA 4
+
+#Conexiones SQL
+DB_Northwind <- DBI::dbConnect(odbc::odbc(),
+                                Driver = "SQL Server",
+                                Server = "dbtedamssql.uh.ac.cr",
+                                Database = "Northwind",
+                                UID = rstudioapi::askForPassword("Database user"),
+                                PWD = rstudioapi::askForPassword("Database password"),
+                                Port = 1433)
+
+DB_Northwind=dbGetQuery(DB_Northwind,"select * from [dbo].[customers]")
+View(DB_Northwind)
+
+#Dplyr
+
+Agrupacion = DB_Northwind%>%group_by(state,City)%>% 
+  summarise(Cantidad_Ciudad=n())
+
+View(Ciudad)
